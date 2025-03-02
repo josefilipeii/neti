b2953905f4550edb8e19453f19954623f681e8af
José Filipe Chavarria <jose.filipe.chavarria@gmail.com>
2025-03-02 02:37:18 +0000

    Import storage

--- cloud/functions/src/file-upload/on-competitions.ts
+++ cloud/functions/src/file-upload/on-competitions.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {Category, Competition} from "@shared";
+import {Storage} from "firebase-admin/lib/storage";
 
 admin.initializeApp();
 const db = admin.firestore();
